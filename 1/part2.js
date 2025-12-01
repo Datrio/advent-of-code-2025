1cc4537bf8c1cd8f18ba45a28fd31c8fe38e9080
Dariusz Siedlecki <dariusz.siedlecki@getyourguide.com>
2025-12-01 10:29:46 +0100

    small optim in part 2

--- 1/part2.js
+++ 1/part2.js
@@ -6,17 +6,21 @@ const lines = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'ut
 let curNumber = 50
 let zeroCount = 0
 
+const floor = (val) => Math.floor(val / 100)
+
 lines.forEach(l => {
   const [dir, count] = [l[0], +l.slice(1)]
   const val = (dir === 'L') ? -count : count
-  
+
   if (val > 0) {
-    zeroCount += Math.floor((curNumber + val) / 100) - Math.floor(curNumber / 100)
+    zeroCount += floor(curNumber + val) - floor(curNumber)
   } else if (val < 0) {
-    zeroCount += Math.floor((curNumber - 1) / 100) - Math.floor((curNumber + val - 1) / 100)
+    let negVal = curNumber - 1
+
+    zeroCount += floor(negVal) - floor(negVal + val)
   }
 
-  curNumber = (((curNumber + val) % 100) + 100) % 100
+  curNumber = (curNumber + val) % 100
 })
 
 console.log(zeroCount)
