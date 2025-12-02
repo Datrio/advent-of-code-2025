6cb84249bd78856c142ebcb9e88ce4019817a71f
Dariusz Siedlecki <dariusz.siedlecki@getyourguide.com>
2025-12-02 11:53:07 +0100

    day 2 part 2

--- 2/part1.js
+++ 2/part1.js
@@ -7,19 +7,19 @@ const lines = fs.readFileSync(path.join(__dirname, filename), { encoding: 'utf8'
 let sum = 0
 
 lines.forEach(l => {
-    const [from, to] = l.split('-').map(Number)
+  const [from, to] = l.split('-').map(Number)
+  
+  for (let i = from; i <= to; i++) {
+    const nString = i.toString()
     
-    for (let i = from; i <= to; i++) {
-        const nString = i.toString()
-
-        if (nString.length % 2 !== 0) continue
-
-        let strLength = nString.length/2
-
-        if (nString.slice(0, strLength) === nString.slice(strLength)) {
-            sum += i * 1
-        }
+    if (nString.length % 2 !== 0) continue
+    
+    let strLength = nString.length/2
+    
+    if (nString.slice(0, strLength) === nString.slice(strLength)) {
+      sum += i
     }
+  }
 })
 
 console.log(sum)
--- 2/part2.js
+++ 2/part2.js
@@ -2,4 +2,22 @@ const fs = require('fs')
 const path = require('path')
 
 let filename = process.argv[2] === '1' ? 'input.txt' : 'example.txt'  
-const lines = fs.readFileSync(path.join(__dirname, filename), { encoding: 'utf8' }).trim().split('\n')
+const lines = fs.readFileSync(path.join(__dirname, filename), { encoding: 'utf8' }).trim().split(',')
+
+let sum = 0
+
+const rx = /^(\d+)\1+$/
+
+lines.forEach(l => {
+  const [from, to] = l.split('-').map(Number)
+  
+  for (let i = from; i <= to; i++) {
+    const nString = i.toString()
+    
+    if (nString.match(rx) === null) continue
+    
+    sum += i
+  }
+})
+
+console.log(sum)
